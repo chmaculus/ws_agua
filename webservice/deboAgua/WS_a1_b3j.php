7d662288890a28b005354ce94fa8dd22208b3536
Christian Maculus <cmaculus@focasoftware.com>
2022-03-10 16:53:11 -0300

    Changes to be committed: 	modified:   webservice/deboAgua/WS_a1_b3j.php

--- webservice/deboAgua/WS_a1_b3j.php
+++ webservice/deboAgua/WS_a1_b3j.php
@@ -30,7 +30,6 @@
 			$array_error = array();
 			
 			while ($REG_VI_AGUA_A_TABLET = sqlsrv_fetch_array($VI_AGUA_A_TABLET, SQLSRV_FETCH_ASSOC)){
-				array_push($array_medicion, $REG_VI_AGUA_A_TABLET);
 				
 				// Test:
 					//$REG_VI_AGUA_A_TABLET = array();
@@ -43,7 +42,6 @@
 				if (is_numeric($valor_ID) == true && intval($valor_ID) >= 0) {
 					if (in_array($valor_ID, $tabla_PK) == false) {
 			
-						$RETURN .= "<MEDICION>";
 						
 						/*ID_MED-ID_MED*/
 						$RETURN .= "<ID_MED>". $valor_ID ."</ID_MED>";
@@ -51,37 +49,37 @@
 						$tabla_PK[] = intval($valor_ID);
 						
 						/*NUM_MED-NUM_MED*/
-						$RETURN .= "<NUM_MED>".trim($REG_VI_AGUA_A_TABLET['NUM_MED'])."</NUM_MED>";
+						$REG_VI_AGUA_A_TABLET['NUM_MED']=trim($REG_VI_AGUA_A_TABLET['NUM_MED']);
 						
 						/*MOD_MED-MOD_MED*/
-						$RETURN .= "<MOD_MED>".trim($REG_VI_AGUA_A_TABLET['MOD_MED'])."</MOD_MED>";
+						$REG_VI_AGUA_A_TABLET['MOD_MED']=trim($REG_VI_AGUA_A_TABLET['MOD_MED']);
 						
 						/*COD-COD*/ // Codigo cliente
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['COD'])) == false) {
 							$REG_VI_AGUA_A_TABLET['COD'] = 0;
 						}
-						$RETURN .= "<COD>".trim($REG_VI_AGUA_A_TABLET['COD'])."</COD>";
 						
 						/*DNI-DNI*/
-						$RETURN .= "<DNI>".trim($REG_VI_AGUA_A_TABLET['DNI'])."</DNI>";	
+						$REG_VI_AGUA_A_TABLET['DNI']=trim($REG_VI_AGUA_A_TABLET['DNI']);
 						
 						/*NOM-NOM*/
-						$RETURN .= "<NOM>".trim($REG_VI_AGUA_A_TABLET['NOM'])."</NOM>";
+						$REG_VI_AGUA_A_TABLET['NOM']=trim($REG_VI_AGUA_A_TABLET['NOM']);
 						
 						/*TEL-TEL*/
-						$RETURN .= "<TEL>".trim($REG_VI_AGUA_A_TABLET['TEL'])."</TEL>";
+						$REG_VI_AGUA_A_TABLET['TEL']=trim($REG_VI_AGUA_A_TABLET['TEL']);
 					
 						/*CALLE-CALLE*/
-						$RETURN .= "<CALLE>".trim($REG_VI_AGUA_A_TABLET['CALLE'])."</CALLE>";
+						$REG_VI_AGUA_A_TABLET['CALLE']=trim($REG_VI_AGUA_A_TABLET['CALLE']);
+						$REG_VI_AGUA_A_TABLET['CALLE']=utf8_encode($REG_VI_AGUA_A_TABLET['CALLE']);
+						log_this("log/ws_a1_b3j_calle.log",date("H:i:s")."\n".$REG_VI_AGUA_A_TABLET['CALLE']."\n\n");
 			
 						/*NUMERO-NUMERO*/
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['NUMERO'])) == false) {
 							$REG_VI_AGUA_A_TABLET['NUMERO'] = 0;
 						}
-						$RETURN .= "<NUMERO>".trim($REG_VI_AGUA_A_TABLET['NUMERO'])."</NUMERO>";
 			
 						/*MZNA*/
-						$RETURN .= "<MZNA>".trim($REG_VI_AGUA_A_TABLET['MZNA'])."</MZNA>";
+						$REG_VI_AGUA_A_TABLET['MZNA']=trim($REG_VI_AGUA_A_TABLET['MZNA']);
 			
 						/*CASA-CASA*/
 						$RETURN .= "<CASA>".trim($REG_VI_AGUA_A_TABLET['CASA'])."</CASA>";	
@@ -90,43 +88,40 @@
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['PISO'])) == false) {
 							$REG_VI_AGUA_A_TABLET['PISO'] = 0;
 						}
-						$RETURN .= "<PISO>".trim($REG_VI_AGUA_A_TABLET['PISO'])."</PISO>";
 			
 						/*DEPTO-DEPTO*/
-						$RETURN .= "<DEPTO>".trim($REG_VI_AGUA_A_TABLET['DEPTO'])."</DEPTO>";
+						$REG_VI_AGUA_A_TABLET['DEPTO']=trim($REG_VI_AGUA_A_TABLET['DEPTO']);
 			
 						/*TORRE-TORRE*/
-						$RETURN .= "<TORRE>".trim($REG_VI_AGUA_A_TABLET['TORRE'])."</TORRE>";
+						$REG_VI_AGUA_A_TABLET['TORRE']=trim($REG_VI_AGUA_A_TABLET['TORRE']);
 			
 						/*RUTA-RUTA*/
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['RUTA'])) == false) {
 							$REG_VI_AGUA_A_TABLET['RUTA'] = 0;
 						}
-						$RETURN .= "<RUTA>".trim($REG_VI_AGUA_A_TABLET['RUTA'])."</RUTA>";
+						$REG_VI_AGUA_A_TABLET['RUTA']=trim($REG_VI_AGUA_A_TABLET['RUTA']);
 			
 						/*ORDEN-ORDEN*/
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['ORDEN'])) == false) {
 							$REG_VI_AGUA_A_TABLET['ORDEN'] = 0;
 						}
-						$RETURN .= "<ORDEN>".trim($REG_VI_AGUA_A_TABLET['ORDEN'])."</ORDEN>";	
 			
 						/*PER-PER*/
 						if (strlen(trim($REG_VI_AGUA_A_TABLET['PER'])) <= 0) {
 							$REG_VI_AGUA_A_TABLET['PER'] = "1900/01";
 						}
-						$RETURN .= "<PER>".trim($REG_VI_AGUA_A_TABLET['PER'])."</PER>";
 			
 						/*LEAN-LEAN*/
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['LEAN'])) == false) {
 							$REG_VI_AGUA_A_TABLET['LEAN'] = 0;
 						}
-						$RETURN .= "<LEAN>".trim($REG_VI_AGUA_A_TABLET['LEAN'])."</LEAN>";
+						$REG_VI_AGUA_A_TABLET['LEAN']=round($REG_VI_AGUA_A_TABLET['LEAN'],0);
 			
 						/*LEAC-LEAC*/
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['LEAC'])) == false) {
 							$REG_VI_AGUA_A_TABLET['LEAC'] = 0;
 						}
-						$RETURN .= "<LEAC>".trim($REG_VI_AGUA_A_TABLET['LEAC'])."</LEAC>";
+						$REG_VI_AGUA_A_TABLET['LEAC']=round($REG_VI_AGUA_A_TABLET['LEAC'],0);
 			
 						/*PROMEDIO-PROMEDIO*/
 						if (is_numeric(trim($REG_VI_AGUA_A_TABLET['PROMEDIO'])) == false) {
@@ -134,12 +129,13 @@
 						} else if (intval(trim($REG_VI_AGUA_A_TABLET['PROMEDIO'])) <= 0) {
 							$REG_VI_AGUA_A_TABLET['PROMEDIO'] = 0;
 						}
-						$RETURN .= "<PROMEDIO>".trim($REG_VI_AGUA_A_TABLET['PROMEDIO'])."</PROMEDIO>";
+						$REG_VI_AGUA_A_TABLET['PROMEDIO']=trim($REG_VI_AGUA_A_TABLET['PROMEDIO']);
+						$REG_VI_AGUA_A_TABLET['PROMEDIO']=round($REG_VI_AGUA_A_TABLET['PROMEDIO'],2);
+
+						array_push($array_medicion, $REG_VI_AGUA_A_TABLET);
 			
-						$RETURN .= "</MEDICION>";
 					}
 					else {
-						$RETURN .= "<ERR>PK: ". intval($valor_ID) ." duplicada</ERR>";
 						$ERROR .= "<br />CODIGO MEDIDOR duplicado : ". intval($valor_ID);
 						//:::work 10/03/2022
 						array_push($array_error, "PK: ". intval($valor_ID) ." duplicada");
@@ -148,15 +144,12 @@
 					}
 				}
 				else {
-					$RETURN .= "<ERR>Formato incorrecto: ". $valor_ID ."</ERR>";
 					$ERROR .= "<br />CODIGO MEDIDOR mal-formado :". $valor_ID;
 					array_push($array_error, "Formato incorrecto: ". $valor_ID);
 				}
 					} // end while
 								
 					sqlsrv_free_stmt($VI_AGUA_A_TABLET);
-					
-					$RETURN .= "</VI_AGUA_A_TABLET>";
 					$arr_rtn=array('CNT'=> $entradas, 'MEDICION' => $array_medicion, 'ERR' => $array_error);
 					$RETURN=json_encode($arr_rtn);
 					//$RETURN = utf8_encode($RETURN);
@@ -172,10 +165,11 @@
 							sqlsrv_query($CONEXION, "ROLLBACK TRANSACTION");
 						}		
 					}
+				
 			}//end while
 			
 			// log_this("log/ws_a1_b1j_return.log",date("H:i:s")."\n".print_r($RETURN,true)."\n\n");
-			 log_this("log/ws_a1_b3j_array.log",date("H:i:s")."\n".print_r($array_medicion,true)."\n\n");
+			 log_this("log/ws_a1_b3j_medicion.log",date("H:i:s")."\n".print_r($array_medicion,true)."\n\n");
 			 log_this("log/ws_a1_b3j_error.log",date("H:i:s")."\n".print_r($array_error,true)."\n\n");
 			 log_this("log/ws_a1_b3j_return.log",date("H:i:s")."\n".print_r($RETURN,true)."\n\n");
 			// log_this("log/ws_a1_b1j_arrayn.log",date("H:i:s")."\n".print_r($array_n,true)."\n\n");
