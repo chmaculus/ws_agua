1ae3ac277b1cca2948364cd490d247d9adbf59bb
Christian Maculus <cmaculus@focasoftware.com>
2022-05-13 17:10:54 -0300

    Changes to be committed: 	modified:   webservice/deboAgua/WS_a1_b16j.php

--- webservice/deboAgua/WS_a1_b16j.php
+++ webservice/deboAgua/WS_a1_b16j.php
@@ -43,12 +43,154 @@ $aa=split("/",$data["PERIODO"]);
 $temp=$aa[1]."/".$aa[0];
 $data["PERIODO"]=$temp;
 
+$string_fecha=$fecha_toma." ".$data['HORA_TOMA'];
 
 
 
 
 
 
+
+
+
+
+
+
+#------------------------------------------------------------------
+//verifica si el registro ya existe
+if($data["ID_MED"]!="" and $data["PERIODO"]!=""){
+	// Test para saber si ya existe la entrada en la BDD:
+	$query1 = "SELECT * FROM AGUA_MEDICION WHERE ID_MED = '".$data["ID_MED"]."' AND PER = '".$data["PERIODO"]."'";
+
+	$result = sqlsrv_query($CONEXION, $query1, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+	if(sqlsrv_errors()){
+		log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."error sql 1\n");
+		log_this("log/errores.log",date("H:i:s")."\n".$query1."\n");
+		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
+	}
+  
+
+	//$result = sqlsrv_query($CONEXION, $query1);
+	$rows=sqlsrv_num_rows($result);
+	log_this("log/ws_a1_b16j".date("Ym").".log",date("d H:i:s")." - query1: ".$query1."\n");
+	log_this("log/ws_a1_b16j".date("Ym").".log",date("d H:i:s")." - rows: ".$rows."\n");
+}
+#------------------------------------------------------------------
+log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."pasa verifica registro\n");
+
+
+
+
+
+
+
+if($rows>0){
+			$array=array(	
+				"MODULO" => "AGUA",
+				"ACCION" => "EXPORT_DATA",
+				"ID_MED" => '"'.$data["ID_MED"].'"',
+				"PERIODO" => '"'.$data["PERIODO"].'"',
+				"MENSAJE" => "YA EXISTE UNA MEDICION PARA ESTE PERIODO",
+			);
+		$json=json_encode($array);
+		echo str_replace('\\"','',$json);
+		exit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------
+//no existe registro
+if($rows<1){
+	log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."no existe registro inserta\n");
+	$SQL = "INSERT INTO AGUA_MEDICION 
+				(ID_MED, PER, LEAN, LEAC, VAL, FECHA_TOMA, ID_ERROR, OBSERVACION, ID_OPE, MODO, AUTORIZADO, PATH_FOTO, ID_TABLET) 
+			VALUES 
+				('".$data["ID_MED"]."', '".$data["PERIODO"]."', '".$data['LEAN']."', '".$data['LEAC']."', -1, '".$string_fecha."', 
+					'".$data['ID_ERROR']."', '".$data['OBSERVACION']."', '".$data['ID_OPE']."', 'A', '0', '".$path.$nombre."', '".$data['ID_TABLET']."')";
+
+	log_this("log/sql".date("Y-m").".log",$SQL."\n\n");
+	$result = sqlsrv_query( $CONEXION, $SQL);
+
+	if(sqlsrv_errors()){
+		log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."error sql 2\n");
+		log_this("log/errores.log",date("H:i:s")."\n".$SQL."\n");
+		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
+	}
+
+	$affected=sqlsrv_rows_affected($result);
+	sqlsrv_commit($CONEXION);
+
+	
+	log_this("log/sql".date("Y-m").".log",date("d H:i:s ")." affected ".$affected." -\n");
+
+	if(!isset($result)){
+		log_this("log/sql".date("Y-m").".log",date("d H:i:s").' - error insertar ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].' \n');
+		/* agregar id_table y N de serie tablet		*/
+		$array=array(	
+			"MODULO" => "AGUA",
+			"ACCION" => "EXPORT_DATA",
+			"ID_MED" => '"'.$data["ID_MED"].'"',
+			"PERIODO" => '"'.$data["PERIODO"].'"',
+			"ERROR" => "Error al insertar registro"
+		);
+		$json=json_encode($array);
+		echo str_replace('\\"','',$json);
+		exit;
+	}
+
+
+	if(isset($result)){
+		log_this("log/sql".date("Y-m").".log",date("d H:i:s").'se inserto correctamente - ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].'\n');
+		/* agregar id_table y N de serie tablet		*/
+			$array=array(	
+				"MODULO" => "AGUA",
+				"ACCION" => "EXPORT_DATA",
+				"ID_MED" => '"'.$data["ID_MED"].'"',
+				"PERIODO" => '"'.$data["PERIODO"].'"',
+				"MENSAJE" => "Se inserto correctamente",
+				"REGISTROS_AFECTADOS" => "$affected"
+			);
+		$json=json_encode($array);
+	}
+}
+#------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
 log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."inicio graba temp\n");
 #-------------------------------------------------------------------
 ///graba temporal imagen
@@ -127,174 +269,107 @@ log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."pasa estampar\n");
 #elimino temporal
 log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."elimina temporal\n");
 unlink($nom_temp);
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
 
 
 
-#------------------------------------------------------------------
-//verifica si el registro ya existe
-if($data["ID_MED"]!="" and $data["PERIODO"]!=""){
-	// Test para saber si ya existe la entrada en la BDD:
-	$query1 = "SELECT * FROM AGUA_MEDICION WHERE ID_MED = '".$data["ID_MED"]."' AND PER = '".$data["PERIODO"]."'";
-
-	$result = sqlsrv_query($CONEXION, $query1, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	if(sqlsrv_errors()){
-		log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."error sql 1\n");
-		log_this("log/errores.log",date("H:i:s")."\n".$query1."\n");
-		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
-	}
-  
-
-	//$result = sqlsrv_query($CONEXION, $query1);
-	$rows=sqlsrv_num_rows($result);
-	log_this("log/ws_a1_b16j".date("Ym").".log",date("d H:i:s")." - query1: ".$query1."\n");
-	log_this("log/ws_a1_b16j".date("Ym").".log",date("d H:i:s")." - rows: ".$rows."\n");
-}
-#------------------------------------------------------------------
-log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."pasa verifica registro\n");
+echo str_replace('\\"','',$json);
+exit;
 
 
 
 
-$string_fecha=$fecha_toma." ".$data['HORA_TOMA'];
 
 
-#------------------------------------------------------------------
-//no existe registro
-if($rows<1){
-	log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."no existe registro inserta\n");
-	$SQL = "INSERT INTO AGUA_MEDICION 
-				(ID_MED, PER, LEAN, LEAC, VAL, FECHA_TOMA, ID_ERROR, OBSERVACION, ID_OPE, MODO, AUTORIZADO, PATH_FOTO, ID_TABLET) 
-			VALUES 
-				('".$data["ID_MED"]."', '".$data["PERIODO"]."', '".$data['LEAN']."', '".$data['LEAC']."', -1, '".$string_fecha."', 
-					'".$data['ID_ERROR']."', '".$data['OBSERVACION']."', '".$data['ID_OPE']."', 'A', '0', '".$path.$nombre."', '".$data['ID_TABLET']."')";
-
-	log_this("log/sql".date("Y-m").".log",$SQL."\n\n");
-	$result = sqlsrv_query( $CONEXION, $SQL);
-
-	if(sqlsrv_errors()){
-		log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."error sql 2\n");
-		log_this("log/errores.log",date("H:i:s")."\n".$SQL."\n");
-		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
-	}
-
-	$affected=sqlsrv_rows_affected($result);
-	sqlsrv_commit($CONEXION);
-
-	
-	log_this("log/sql".date("Y-m").".log",date("d H:i:s ")." affected ".$affected." -\n");
-
-	if(!isset($result)){
-		log_this("log/sql".date("Y-m").".log",date("d H:i:s").' - error insertar ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].' \n');
-		/* agregar id_table y N de serie tablet		*/
-		$array=array(	
-			"MODULO" => "AGUA",
-			"ACCION" => "EXPORT_DATA",
-			"ID_MED" => '"'.$data["ID_MED"].'"',
-			"PERIODO" => '"'.$data["PERIODO"].'"',
-			"ERROR" => "Error al insertar registro"
-		);
-		$json=json_encode($array);
-		echo str_replace('\\"','',$json);
-		exit;
-	}
 
 
-	if(isset($result)){
-		log_this("log/sql".date("Y-m").".log",date("d H:i:s").'se inserto correctamente - ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].'\n');
-		/* agregar id_table y N de serie tablet		*/
-			$array=array(	
-				"MODULO" => "AGUA",
-				"ACCION" => "EXPORT_DATA",
-				"ID_MED" => '"'.$data["ID_MED"].'"',
-				"PERIODO" => '"'.$data["PERIODO"].'"',
-				"MENSAJE" => "Se inserto correctamente",
-				"REGISTROS_AFECTADOS" => "$affected"
-			);
-		$json=json_encode($array);
-		echo str_replace('\\"','',$json);
-		exit;
-	}
-}
-#------------------------------------------------------------------
+
+
+
+
 
 
 
 	
-#------------------------------------------------------------------
-//ya existe el registro. actualizo
-if($rows>0){
-	log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."existe registro actualiza\n");
-	$SQL = "update AGUA_MEDICION set 
-								LEAN='".$data['LEAN']."',
-								LEAC='".$data['LEAC']."', 
-								VAL='-1', 
-								FECHA_TOMA='".$string_fecha."', 
-								ID_ERROR='".$data['ID_ERROR']."', 
-								OBSERVACION='".$data['OBSERVACION']."', 
-								ID_OPE='".$data['ID_OPE']."', 
-								MODO='A', 
-								AUTORIZADO='0', 
-								PATH_FOTO='".$path.$nombre."',
-								ID_TABLET='".$data['ID_TABLET']."'
-									where ID_MED='".$data["ID_MED"]."' and 
-											PER='".$data["PERIODO"]."' 
+// #------------------------------------------------------------------
+// //ya existe el registro. actualizo
+// if($rows>0){
+// 	log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."existe registro actualiza\n");
+// 	$SQL = "update AGUA_MEDICION set 
+// 								LEAN='".$data['LEAN']."',
+// 								LEAC='".$data['LEAC']."', 
+// 								VAL='-1', 
+// 								FECHA_TOMA='".$string_fecha."', 
+// 								ID_ERROR='".$data['ID_ERROR']."', 
+// 								OBSERVACION='".$data['OBSERVACION']."', 
+// 								ID_OPE='".$data['ID_OPE']."', 
+// 								MODO='A', 
+// 								AUTORIZADO='0', 
+// 								PATH_FOTO='".$path.$nombre."',
+// 								ID_TABLET='".$data['ID_TABLET']."'
+// 									where ID_MED='".$data["ID_MED"]."' and 
+// 											PER='".$data["PERIODO"]."' 
 
-					";
+// 					";
 
 
-	log_this("log/sql".date("Y-m").".log",$SQL."\n");
+// 	log_this("log/sql".date("Y-m").".log",$SQL."\n");
 	
-	$RESP_UPDATE = sqlsrv_query($CONEXION, $SQL);
+// 	$RESP_UPDATE = sqlsrv_query($CONEXION, $SQL);
 
-	if(sqlsrv_errors()){
-		log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."error sql 3\n");
-		log_this("log/errores.log",date("H:i:s")."\n".$SQL."\n");
-		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
-	}
+// 	if(sqlsrv_errors()){
+// 		log_this("log/ws_a1_b16j".date("Ym").".log", date("d H:i:s")."error sql 3\n");
+// 		log_this("log/errores.log",date("H:i:s")."\n".$SQL."\n");
+// 		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
+// 	}
 
-	$affected=sqlsrv_rows_affected($RESP_UPDATE);
-	sqlsrv_commit($CONEXION);
+// 	$affected=sqlsrv_rows_affected($RESP_UPDATE);
+// 	sqlsrv_commit($CONEXION);
 	
-	if(sqlsrv_errors()){
-		log_this("log/errores.log",date("H:i:s")."\n".$query1."\n");
-		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
-	}
+// 	if(sqlsrv_errors()){
+// 		log_this("log/errores.log",date("H:i:s")."\n".$query1."\n");
+// 		log_this("log/errores.log",date("H:i:s")."\n".print_r( sqlsrv_errors(), true));
+// 	}
 
 
-	log_this("log/sql".date("Y-m").".log",date("d H:i:s ").print_r($RESP_UPDATE,true)." \n");
-	log_this("log/sql".date("Y-m").".log",date("d H:i:s ")." affected ".$affected." \n");
+// 	log_this("log/sql".date("Y-m").".log",date("d H:i:s ").print_r($RESP_UPDATE,true)." \n");
+// 	log_this("log/sql".date("Y-m").".log",date("d H:i:s ")." affected ".$affected." \n");
 
-	if(!isset($RESP_UPDATE)){
-				log_this("log/sql".date("Y-m").".log",date("d H:i:s").' - error actualizar ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].'\n');
-			$array=array(	
-				"MODULO" => "AGUA",
-				"ACCION" => "EXPORT_DATA",
-				"ID_MED" => '"'.$data["ID_MED"].'"',
-				"PERIODO" => '"'.$data["PERIODO"].'"',
-				"ERROR" => "Error al actualizar registro"
-			);
-		$json=json_encode($array);
-		echo str_replace('\\"','',$json);
-		exit;
-	}
+// 	if(!isset($RESP_UPDATE)){
+// 				log_this("log/sql".date("Y-m").".log",date("d H:i:s").' - error actualizar ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].'\n');
+// 			$array=array(	
+// 				"MODULO" => "AGUA",
+// 				"ACCION" => "EXPORT_DATA",
+// 				"ID_MED" => '"'.$data["ID_MED"].'"',
+// 				"PERIODO" => '"'.$data["PERIODO"].'"',
+// 				"ERROR" => "Error al actualizar registro"
+// 			);
+// 		$json=json_encode($array);
+// 		echo str_replace('\\"','',$json);
+// 		exit;
+// 	}
 
-	if(isset($RESP_UPDATE)){
-		log_this("log/sql".date("Y-m").".log",date("d H:i:s").'update ok - ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].'\n');
-			$array=array(	
-				"MODULO" => "AGUA",
-				"ACCION" => "EXPORT_DATA",
-				"ID_MED" => '"'.$data["ID_MED"].'"',
-				"PERIODO" => '"'.$data["PERIODO"].'"',
-				"MENSAJE" => "Se actualizo correctamente",
-				"REGISTROS_AFECTADOS" => '"'.$affected.'"'
-			);
-		$json=json_encode($array);
-		echo str_replace('\\"','',$json);
-			exit;
-	}
+// 	if(isset($RESP_UPDATE)){
+// 		log_this("log/sql".date("Y-m").".log",date("d H:i:s").'update ok - ID_MED - '.$data["ID_MED"].' - periodo - '.$data["PERIODO"].'\n');
+// 			$array=array(	
+// 				"MODULO" => "AGUA",
+// 				"ACCION" => "EXPORT_DATA",
+// 				"ID_MED" => '"'.$data["ID_MED"].'"',
+// 				"PERIODO" => '"'.$data["PERIODO"].'"',
+// 				"MENSAJE" => "Se actualizo correctamente",
+// 				"REGISTROS_AFECTADOS" => '"'.$affected.'"'
+// 			);
+// 		$json=json_encode($array);
+// 		echo str_replace('\\"','',$json);
+// 			exit;
+// 	}
 
-}
-#------------------------------------------------------------------
+// }
+// #------------------------------------------------------------------
 
 
 
